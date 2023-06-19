16022516f7754dd91632fe8954fad1565299fa03
Mohamed Nasser <mohamed17nasser@gmail.com>
2023-06-19 20:58:02 +0300

    first commit!
    Basic Project structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+      <p className="text-xl mb-4">Count: {count}</p>
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+      <div className="rating">
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+          checked
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
