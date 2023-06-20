6ed495d5da6049fbcc68e7999919476ef93aefc9
bavly-ashraf <bavly.ashraf2023@gmail.com>
2023-06-21 02:56:38 +0300

    range function in utils

--- src/pages/productsListing/ProductsListing.jsx
+++ src/pages/productsListing/ProductsListing.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {range} from '../../utils/range';
 
 const ProductListing = () => {
+
+    // filteration
+
+    // search
+
+    // pagination
+    const PAGE_SIZE = 9;
+    console.log(range(5));
+
+
     return ( 
         <>
         <h1 className='mx-auto'>Category Name</h1>
