bb7d5458de61727045325f085155109d550d0d70
bavly-ashraf <bavly.ashraf2023@gmail.com>
2023-06-21 20:22:49 +0300

    ProductListing Page Edits

--- src/pages/productsListing/ProductsListing.jsx
+++ src/pages/productsListing/ProductsListing.jsx
@@ -17,35 +17,91 @@ const ProductListing = () => {
 
     return ( 
         <>
-        <h1 className='text-5xl text-center text-black m-3'>Category Name</h1>
-        <div className='flex'>
-            <aside>
-                <div className="drawer lg:drawer-open">
+        <h1 className='text-5xl text-center text-black m-3 dark:text-white'>Category Name</h1>
+        <div className='flex justify-end me-32'>
+        <details className="dropdown">
+            <summary className="m-1 btn hover:bg-project-main-color hover:text-black">sort by</summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                <li><a>Item 1</a></li>
+                <li><a>Item 2</a></li>
+            </ul>
+        </details>
+        </div>
+        <div className='flex justify-between'>
+            <aside className='ms-3'>
+                <div className="drawer h-full lg:drawer-open">
                     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                    <div className="drawer-content flex flex-col items-center justify-center">
-                        {/* Page content here */}
-                        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    <div className="drawer-content">
+                        <label htmlFor="my-drawer-2" className="btn border-2 border-project-main-color hover:text-white hover:bg-project-main-color hover:border-project-main-color text-black drawer-button mx-2 lg:hidden">Filter by</label>
                     
                     </div> 
                     <div className="drawer-side">
                         <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
                         <ul className="menu bg-base-200 w-56 rounded-box">
+                            <div className="divider"></div> 
                             <li>
                                 <details open>
-                                <summary>Parent</summary>
+                                <summary className='text-xl'>Item Type</summary>
                                 <ul>
-                                    <li><a>level 2 item 1</a></li>
-                                    <li><a>level 2 item 2</a></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Coffee Table</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Media Console</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Desk</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Desk Chair</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Console</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Table Lamp</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Floor Lamp</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Bookcase</p></li>
+                                    <li><p><input type='checkbox' className='cursor-pointer'></input>Rug</p></li>
                                 </ul>
                                 </details>
                             </li>
+                            <div className="divider"></div>
+                            <li>
+                                <details open>
+                                <summary className='text-xl'>Brand</summary>
+                                <ul>
+                                <li><p><input type='checkbox' className='cursor-pointer'></input>Feather</p></li>
+                                <li><p><input type='checkbox' className='cursor-pointer'></input>West Elm</p></li>
+                                <li><p><input type='checkbox' className='cursor-pointer'></input>Floyd</p></li>
+                                <li><p><input type='checkbox' className='cursor-pointer'></input>Fully</p></li>
+                                <li><p><input type='checkbox' className='cursor-pointer'></input>Herman Miller</p></li>
+                                </ul>
+                                </details>
+                            </li>
+                            <div className="divider"></div>
+                            <li>
+                                <details open>
+                                <summary className='text-xl'>Price</summary>
+                                <ul>
+                                    <li><input className='h-10 border-2 border-gray-500 focus:border-black w-8/12 p-3 m-auto mb-3' type='number' placeholder='min' /></li>
+                                    <li><input className='h-10 border-2 border-gray-500 focus:border-black w-8/12 p-3 m-auto' type='number' placeholder='max' /></li>
+                                </ul>
+                                </details>
+                            </li>
+                            <div className="divider"></div>
+                            <li>
+                                <details open>
+                                <summary className='text-xl'>Shop by category</summary>
+                                <ul>
+                                    <li><a>Living Room</a></li>
+                                    <li><a>Bedroom</a></li>
+                                    <li><a>Dining</a></li>
+                                    <li><a>Home Office</a></li>
+                                    <li><a>Decor</a></li>
+                                    <li><a>Lighting</a></li>
+                                    <li><a>Outdoor</a></li>
+                                </ul>
+                                </details>
+                            </li>
+                            <div className="divider"></div>
+                            <a className='text-lg cursor-pointer underline'>Or, browse all furniture</a>
                         </ul>
                     </div>
                 </div>
-                <div className="divider"></div> 
+                
             </aside>
-            <main>
-                <div className='flex flex-wrap gap-4'>
+            <main className='flex flex-col items-center mx-4'>
+                <div className='flex flex-wrap justify-center gap-4'>
                 <Product />
                 <Product />
                 <Product />
@@ -56,11 +112,11 @@ const ProductListing = () => {
                 <Product />
                 <Product />
                 </div>
-                <div className="join">
-                    <input className="join-item btn btn-square" type="radio" name="options" aria-label="1" />
-                    <input className="join-item btn btn-square" type="radio" name="options" aria-label="2" />
-                    <input className="join-item btn btn-square" type="radio" name="options" aria-label="3" />
-                    <input className="join-item btn btn-square" type="radio" name="options" aria-label="4" />
+                <div className="join my-4">
+                    <input className="join-item btn btn-square checked:!bg-project-main-color checked:!border-project-main-color" type="radio" name="options" aria-label="1" />
+                    <input className="join-item btn btn-square checked:!bg-project-main-color checked:!border-project-main-color" type="radio" name="options" aria-label="2" />
+                    <input className="join-item btn btn-square checked:!bg-project-main-color checked:!border-project-main-color" type="radio" name="options" aria-label="3" />
+                    <input className="join-item btn btn-square checked:!bg-project-main-color checked:!border-project-main-color" type="radio" name="options" aria-label="4" />
                 </div>
             </main>
         </div>
