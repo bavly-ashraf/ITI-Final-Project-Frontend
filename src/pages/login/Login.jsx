d6785d67e9db9a1c72a192178b9ec785c5962e1c
Mohamed Nasser <mohamed17nasser@gmail.com>
2023-06-23 17:12:38 +0300

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -151,9 +151,9 @@ export default function App() {
                   className="password-toggle-icon absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
                 >
                   {showPassword ? (
-                    <EyeIcon className="h-5 w-5 text-gray-500" />
+                    <EyeIcon className="h-5 w-5 text-stone-500" />
                   ) : (
-                    <EyeSlashIcon className="h-5 w-5 text-gray-500" />
+                    <EyeSlashIcon className="h-5 w-5 text-stone-500" />
                   )}
                 </span>
               </div>
