194108e115739a1b2cefa706f80d11ce68d70599
kareem132sameir <mangaka13215@gmail.com>
2023-06-23 18:50:11 +0300

    added pagination to admin dashboard

--- src/components/ordersTable/OrdersTable.jsx
+++ src/components/ordersTable/OrdersTable.jsx
@@ -1,81 +1,118 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const OrdersTable = (props) => {
   let orders = props.orders;
   let keys;
-  if(orders.length!=0)
-  {
-      keys = Object.keys(orders[0]);
+  if (orders.length != 0) {
+    keys = Object.keys(orders[0]);
+  }
+
+  let [currentPage, setCurrentPage] = useState(0);
+  let [startOrder, setStartOrder] = useState(0);
+  let noOfOrders = 8;
+  let endOrder = startOrder + noOfOrders - 1;
+  let pages = Math.floor(orders.length / noOfOrders);
+  pages++;
+
+  function paginate(e, index) {
+    setCurrentPage(index);
+    setStartOrder(index * noOfOrders);
+    endOrder = startOrder + noOfOrders - 1;
   }
 
   return (
     <>
-      {orders.length!=0 ? (
-        <div className=" bg-[#FBF8F5] overflow-x-auto">
-          <table className="table">
-            <thead className="bg-[#F3F0EC]">
-              <tr>
-                {keys.map((key) => (
-                  <th className="text-black text-lg" key={key}>
-                    {key}
-                  </th>
-                ))}
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-              {orders.map((user) => (
-                <tr key={user.OrderId} className="hover:bg-[#F3F0EC]">
-                  <td>
-                    <div className="">{user.UserId}</div>
-                  </td>
-                  <td>
-                    <div className="">{user.Order_Date}</div>
-                  </td>
-                  <td>
-                    <div className="">{user.OrderId}</div>
-                  </td>
-                  <td>
-                    <div className="">{user.Total_Price}$</div>
-                  </td>
-                  <td>
-                    <div className="">
-                      {user.Shipping_Address.city},
-                      {user.Shipping_Address.country},
-                      {user.Shipping_Address.zip}
-                    </div>
-                  </td>
-                  <th>
-                    <button
-                      onClick={() => {
-                        props.deleteOrder(user.OrderId);
-                      }}
-                      className="btn btn-ghost btn-xs rounded-full h-10 w-24 hover:bg-[#E9672B]"
-                    >
-                      Confirm
-                    </button>
-                  </th>
-                  <th>
-                    <button
-                      onClick={() => {
-                        props.deleteOrder(user.OrderId);
-                      }}
-                      className="btn btn-ghost btn-xs rounded-full h-10 w-24 hover:bg-[#E9672B]"
-                    >
-                      Delete
-                    </button>
-                  </th>
+      {orders.length != 0 ? (
+        <>
+          <div className=" bg-[#FBF8F5] overflow-x-auto h-4/5">
+            <table className="table">
+              <thead className="bg-[#F3F0EC]">
+                <tr>
+                  {keys.map((key) => (
+                    <th className="text-black text-lg" key={key}>
+                      {key}
+                    </th>
+                  ))}
+                  <th></th>
+                  <th></th>
                 </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {orders.map((user, index) => {
+                  return index >= startOrder && index <= endOrder ? (
+                    <>
+                      <tr key={user.OrderId} className="hover:bg-[#F3F0EC]">
+                        <td>
+                          <div className="">{user.UserId}</div>
+                        </td>
+                        <td>
+                          <div className="">{user.Order_Date}</div>
+                        </td>
+                        <td>
+                          <div className="">{user.OrderId}</div>
+                        </td>
+                        <td>
+                          <div className="">{user.Total_Price}$</div>
+                        </td>
+                        <td>
+                          <div className="">
+                            {user.Shipping_Address.city},
+                            {user.Shipping_Address.country},
+                            {user.Shipping_Address.zip}
+                          </div>
+                        </td>
+                        <th>
+                          <button
+                            onClick={() => {
+                              props.deleteOrder(user.OrderId);
+                            }}
+                            className="btn btn-ghost btn-xs rounded-full h-10 w-24 hover:bg-[#E9672B]"
+                          >
+                            Confirm
+                          </button>
+                        </th>
+                        <th>
+                          <button
+                            onClick={() => {
+                              props.deleteOrder(user.OrderId);
+                            }}
+                            className="btn btn-ghost btn-xs rounded-full h-10 w-24 hover:bg-[#E9672B]"
+                          >
+                            Delete
+                          </button>
+                        </th>
+                      </tr>
+                    </>
+                  ) : (
+                    ""
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-center">
+            <div className="join">
+              {Array.from({ length: pages }).map((_, index) => (
+                <input
+                  key={index}
+                  className="join-item btn btn-square"
+                  type="radio"
+                  name="options"
+                  onClick={(event) => paginate(event, index)}
+                  aria-label={index + 1}
+                  checked={index === currentPage}
+                />
               ))}
-            </tbody>
-          </table>
-        </div>
+            </div>
+          </div>
+        </>
       ) : (
         <div className="flex justify-center align-middle h-screen w-full">
-            <div className="loading loading-lg loading-spinner text-[#E9672B] "></div>
+          <div className="loading loading-lg loading-spinner text-[#E9672B] "></div>
         </div>
       )}
     </>
--- src/pages/adminDashboard/AdminDashboard.jsx
+++ src/pages/adminDashboard/AdminDashboard.jsx
@@ -6,7 +6,7 @@ import OrdersTable from "../../components/ordersTable/OrdersTable";
 const AdminDashBoard = () => {
   const [orders, setOrders] = useState([
     {
-      UserId: "USR123",
+      UserId: "1",
       Order_Date: "2023-06-19",
       OrderId: "ORD456",
       Total_Price: 39.99,
@@ -17,7 +17,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR789",
+      UserId: "2",
       Order_Date: "2023-06-18",
       OrderId: "ORD012",
       Total_Price: 25.99,
@@ -28,7 +28,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR345",
+      UserId: "3",
       Order_Date: "2023-06-17",
       OrderId: "ORD678",
       Total_Price: 12.5,
@@ -39,7 +39,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR901",
+      UserId: "4",
       Order_Date: "2023-06-16",
       OrderId: "ORD234",
       Total_Price: 99.99,
@@ -50,7 +50,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR567",
+      UserId: "5",
       Order_Date: "2023-06-15",
       OrderId: "ORD890",
       Total_Price: 49.99,
@@ -61,7 +61,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR234",
+      UserId: "6",
       Order_Date: "2023-06-14",
       OrderId: "ORD567",
       Total_Price: 14.99,
@@ -72,7 +72,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR678",
+      UserId: "7",
       Order_Date: "2023-06-13",
       OrderId: "ORD123",
       Total_Price: 9.99,
@@ -83,7 +83,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR012",
+      UserId: "8",
       Order_Date: "2023-06-12",
       OrderId: "ORD789",
       Total_Price: 19.99,
@@ -94,7 +94,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR456",
+      UserId: "9",
       Order_Date: "2023-06-11",
       OrderId: "ORD345",
       Total_Price: 29.99,
@@ -105,7 +105,7 @@ const AdminDashBoard = () => {
       },
     },
     {
-      UserId: "USR789",
+      UserId: "10",
       Order_Date: "2023-06-10",
       OrderId: "ORD901",
       Total_Price: 34.99,
@@ -115,7 +115,118 @@ const AdminDashBoard = () => {
         country: "United States",
       },
     },
-  ]);
+    {
+      UserId: "11",
+      Order_Date: "2023-06-09",
+      OrderId: "ORD457",
+      Total_Price: 39.99,
+      Shipping_Address: {
+        city: "New York",
+        zip: "10001",
+        country: "United States",
+      },
+    },
+    {
+      UserId: "12",
+      Order_Date: "2023-06-08",
+      OrderId: "ORD013",
+      Total_Price: 25.99,
+      Shipping_Address: {
+        city: "London",
+        zip: "WC1A 1AA",
+        country: "United Kingdom",
+      },
+    },
+    {
+      UserId: "13",
+      Order_Date: "2023-06-07",
+      OrderId: "ORD679",
+      Total_Price: 12.5,
+      Shipping_Address: {
+        city: "Paris",
+        zip: "75000",
+        country: "France",
+      },
+    },
+    {
+      UserId: "14",
+      Order_Date: "2023-06-06",
+      OrderId: "ORD235",
+      Total_Price: 99.99,
+      Shipping_Address: {
+        city: "Tokyo",
+        zip: "100-0001",
+        country: "Japan",
+      },
+    },
+    {
+      UserId: "15",
+      Order_Date: "2023-06-05",
+      OrderId: "ORD891",
+      Total_Price: 49.99,
+      Shipping_Address: {
+        city: "Sydney",
+        zip: "2000",
+        country: "Australia",
+      },
+    },
+    {
+      UserId: "16",
+      Order_Date: "2023-06-04",
+      OrderId: "ORD568",
+      Total_Price: 14.99,
+      Shipping_Address: {
+        city: "Berlin",
+        zip: "10115",
+        country: "Germany",
+      },
+    },
+    {
+      UserId: "17",
+      Order_Date: "2023-06-03",
+      OrderId: "ORD124",
+      Total_Price: 9.99,
+      Shipping_Address: {
+        city: "Toronto",
+        zip: "M5V 2L7",
+        country: "Canada",
+      },
+    },
+    {
+      UserId: "18",
+      Order_Date: "2023-06-02",
+      OrderId: "ORD790",
+      Total_Price: 19.99,
+      Shipping_Address: {
+        city: "Rome",
+        zip: "00100",
+        country: "Italy",
+      },
+    },
+    {
+      UserId: "19",
+      Order_Date: "2023-06-01",
+      OrderId: "ORD346",
+      Total_Price: 29.99,
+      Shipping_Address: {
+        city: "Moscow",
+        zip: "101000",
+        country: "Russia",
+      },
+    },
+    {
+      UserId: "20",
+      Order_Date: "2023-05-31",
+      OrderId: "ORD902",
+      Total_Price: 34.99,
+      Shipping_Address: {
+        city: "Los Angeles",
+        zip: "90001",
+        country: "United States",
+      },
+    }
+  ]
+  );
 
 
   
