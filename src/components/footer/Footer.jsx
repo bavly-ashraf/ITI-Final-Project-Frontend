4f6cbdf266992cf0f3994fb515f26501791e0533
AlaaShawkyy <alaashawky51@gmail.com>
2023-06-24 03:46:03 +0300

    Alaa added header and footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,38 @@
-import React from "react";
-
-function Footer() {
+import React from 'react'
+import './Footer.css'
+import { Link } from 'react-router-dom'
+export default function Footer() {
   return (
-    <footer className="bg-gray-200 py-4">
-      <p className="text-center text-gray-600 text-sm">
-        &copy; 2023 Your Website. All rights reserved.
-      </p>
-    </footer>
-  );
-}
+<>
+<div className='begin'></div>
+<footer className="footer p-10 text-black">
+  <div>
+    <span className=" font-bold text-black">FURNITURE</span> 
 
-export default Footer;
+    <Link className="cursor-pointer hover:text-gray-500">Living Room</Link>
+    <Link className="cursor-pointer hover:text-gray-500">Bedroom</Link>
+    <Link className="cursor-pointer hover:text-gray-500">Dining</Link>
+    <Link className="cursor-pointer hover:text-gray-500">Home office</Link>
+    <Link className="cursor-pointer hover:text-gray-500">Lightning</Link>
+    <Link className="cursor-pointer hover:text-gray-500">Outdoor</Link>
+
+ 
+
+  </div> 
+  <div>
+    <span className="font-bold text-black">ABOUT</span> 
+    <Link className="cursor-pointer hover:text-gray-500">About us</Link>
+    <Link className="cursor-pointer hover:text-gray-500">Contact</Link>
+
+  </div> 
+  <div>
+    <span className="font-bold text-black">Legal</span> 
+    <Link className="cursor-pointer hover:text-gray-500">Terms</Link>
+    <Link className="cursor-pointer hover:text-gray-500">Privacy policy</Link>
+  </div>
+  
+</footer>
+</>
+
+    )
+  }
