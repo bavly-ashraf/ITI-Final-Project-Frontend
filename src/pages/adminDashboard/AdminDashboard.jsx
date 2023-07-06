a7681caede302569f9fb3b8afbfac53ceda98461
kareem132sameir <mangaka13215@gmail.com>
2023-07-06 15:23:10 +0300

    delete confirmation modal in admin dashboard

--- src/pages/adminDashboard/AdminDashboard.jsx
+++ src/pages/adminDashboard/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useState } from "react";
 import OrdersTable from "../../components/ordersTable/OrdersTable";
+import DeleteModalConfirm from "../../components/deleteModalConfirm/DeleteModalConfirm";
 import axios from "../../api/axios";
 import { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
@@ -232,13 +233,17 @@ const AdminDashBoard = () => {
   // ]
   // );
 
+  const [showModal, setShowModal] = useState(false);
+
+  let [currentId, setCurrentId] = useState(0);
+
   const notify = () =>
     toast.success("Item Deleted Successfully!", {
       position: toast.POSITION.TOP_RIGHT,
     });
 
   const error = () =>
-  toast.error('Error Notification !', {
+    toast.error("Error Notification !", {
       position: toast.POSITION.TOP_RIGHT,
     });
 
@@ -259,6 +264,7 @@ const AdminDashBoard = () => {
   }
   async function deleteItem(id) {
     try {
+      console.log("deleting...");
       const response = await axios.delete(`http://localhost:3000/orders/${id}`);
       notify();
       // Perform additional actions after successful deletion
@@ -287,13 +293,20 @@ const AdminDashBoard = () => {
   }, []);
 
   function deleteOrder(id) {
-    deleteItem(id);
+    setCurrentId(id);
+    setShowModal(true);
+  }
+
+  function deleteItemFromTable(id)
+  {
     let newOrders = [...orders];
     newOrders = newOrders.filter((order) => order._id != id);
     setOrders(newOrders);
   }
   function confirmOrder(id) {
     // updateItem(id);
+    setCurrentId(id);
+    setShowModal(true);
     error();
     let newOrders = [...orders];
     newOrders = newOrders.filter((order) => order._id != id);
@@ -302,12 +315,18 @@ const AdminDashBoard = () => {
 
   return (
     <>
+      {/* <DeleteModalConfirm showModal={showModal} setShowModal={setShowModal} /> */}
       <ToastContainer />
       {orders.length !== 0 ? (
         <OrdersTable
           deleteOrder={deleteOrder}
           confirmOrder={confirmOrder}
           orders={orders}
+          showModal={showModal}
+          setShowModal={setShowModal}
+          currentId={currentId}
+          deleteItem={deleteItem}
+          deleteItemFromTable={deleteItemFromTable}
         />
       ) : (
         <div className="flex justify-center align-middle h-screen w-full">
