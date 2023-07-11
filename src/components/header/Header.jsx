162873c1d6644ec1b904dedc794eb155a5cab25c
AlaaShawkyy <alaashawky51@gmail.com>
2023-07-11 18:29:20 +0300

    header , checkout, cartItem done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -253,13 +253,7 @@ function Header() {
             <div className="dropdown dropdown-end">
               <label tabIndex="0" className="btn btn-ghost btn-circle">
                 <div className="indicator">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 "
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
